1fc6fd92fd2c00285542f1e6c6fa75b8a206e54f
830Utpal <lakshman2016yadav@gmail.com>
2025-03-14 23:37:51 +0530

    auth.jsx

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Background from "@/assets/login2.png";
 import Victory from "@/assets/victory.svg";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const Auth = () => {
+
+  const [email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[confirmPassword,setConfirmPassword]=useState(""); 
+
   return (
     <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
       <div className='h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
